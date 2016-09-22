4629e964184717b7bbf8b7381999d00cc3e90b45
Jian Wei <jianwei@jianwei-deb8-64>
2016-09-22 17:48:08 -0400

    second edit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <memory>
+
+class TreeNode {
+    public:
+        size_t start, end;
+        std::shared_ptr<TreeNode> left, right;
+
+        TreeNode(size_t a_start, size_t a_end) : start(a_start), end(a_end){
+            if (a_start < a_end) {
+                size_t mid = (a_start+a_end)/2;
+                left = std::make_shared<TreeNode>(a_start, mid);
+                right = std::make_shared<TreeNode>(mid+1, a_end);
+            }
+            else {
+                left = nullptr;
+                right = nullptr;
+            }
+        }
+};
 
 class solution {
     public:
         int countRangeSum(std::vector<int> & nums, int lower, int upper) {
-            
+
         }
 };
 
